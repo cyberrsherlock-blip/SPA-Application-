f893ef5bc5906db5db3d3d1d76877dd777df444c
cyberrsherlock-blip <cyberrsherlock@gmail.com>
2026-05-29 13:46:58 +0530

    Create auth.guard.ts

---
+++ frontend/src/app/core/guards/auth.guard.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+import { UserService } from '../services/user.service';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+  constructor(private userService: UserService, private router: Router) {}
+
+  canActivate() {
+    if (this.userService.currentUserValue) return true;
+    this.router.navigate(['/login']);
+    return false;
+  }
+}
