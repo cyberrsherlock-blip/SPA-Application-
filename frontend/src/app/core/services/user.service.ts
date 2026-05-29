aa173a30a7d54c5b8a845fdeae95dabdf286aabc
cyberrsherlock-blip <cyberrsherlock@gmail.com>
2026-05-29 14:33:22 +0530

    Update user.service.ts

--- frontend/src/app/core/services/user.service.ts
+++ frontend/src/app/core/services/user.service.ts
@@ -1,45 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, BehaviorSubject } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { environment } from '../../../environments/environment'; 
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
-  private apiUrl = 'http://localhost:3000/api';
-  private currentUserSubject = new BehaviorSubject<any>(JSON.parse(localStorage.getItem('currentUser') || 'null'));
   
-  public currentUser$ = this.currentUserSubject.asObservable();
-
-  constructor(private http: HttpClient) {}
-
-  public get currentUserValue() {
-    return this.currentUserSubject.value;
-  }
-
-  login(credentials: any, delayMs: number = 1500): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/login?delay=${delayMs}`, credentials).pipe(
-      tap(user => {
-        localStorage.setItem('currentUser', JSON.stringify(user));
-        this.currentUserSubject.next(user);
-      })
-    );
-  }
-
-  logout() {
-    localStorage.removeItem('currentUser');
-    this.currentUserSubject.next(null);
-  }
-
-  getRecords(delayMs: number = 2000): Observable<any[]> {
-    const user = this.currentUserValue;
-    return this.http.get<any[]>(`${this.apiUrl}/records?userId=${user.id}&role=${user.role}&delay=${delayMs}`);
-  }
-
-  getAllUsers(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/users?delay=1000`);
-  }
-
-  deleteUser(id: string): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/users/${id}`);
-  }
+  private apiUrl = environment.apiUrl; 
+  
 }
