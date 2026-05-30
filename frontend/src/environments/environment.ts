95a2fe940fab4fafb1061b3b3edd3e947921b32b
cyberrsherlock-blip <cyberrsherlock@gmail.com>
2026-05-30 20:38:02 +0530

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://github.com/cyberrsherlock-blip/SPA-Application-.git'
+  apiUrl: 'https://cyberrsherlock-blip.github.io/SPA-Application-/'
 };
